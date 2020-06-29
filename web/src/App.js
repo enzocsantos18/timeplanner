ac9a3fa18c10dd31f7a784eb00158f6a63922b12
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2020-06-29 17:09:23 -0300

    Navigation structure

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,18 @@
 import React, {useEffect} from 'react';
+import {Router} from 'react-router-dom'
+import history from './services/history'
+
 import GlobalStyle from './styles';
-import Home from './pages/Home'
+import Routes from './routes'
 function App() {
 
   return (
     <div>
       <GlobalStyle/>
-      <Home></Home>
+      <Router history={history}>
+
+      <Routes></Routes>
+      </Router>
 
     </div>
   );
