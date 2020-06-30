44b7eca69dbe18482533c6ce57875413ecb74584
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2020-06-30 14:37:13 -0300

    Logout feature added

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
 import {Container, ProjectList, SideBar } from './styles';
+import {useDispatch} from 'react-redux';
+import {signOut} from '../../store/modules/auth/actions'
 import Project from '../../components/Project'
 import api from '../../services/api';
 
 
 function Home() {
+  const dispatch = useDispatch();
+
+  function handleSignOut(){
+    dispatch(signOut())
+
+  }
   api.get('project')
   return (
     <Container>
     <SideBar>
       <p>Criar novo projeto</p>
+      <button onClick={handleSignOut}>Sair</button>
     </SideBar>
     <ProjectList>
       <Project></Project>
